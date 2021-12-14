2ae21ef4a73e4815a5cedffeafd51a3bcd3d52d5
Wesley Rogers <wprogers86@gmail.com>
2021-12-13 22:02:04 -0500

    added literal statement"

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const readMe = ({ }) => {
 
+const readMe = ({ title, description, installation, usage, license, contribution, test, github, email }) => {
+
+    const choices={
+        MIT : 'https://img.shields.io/badge/license-MIT-brightgreen',
+        Apache : 'https://img.shields.io/badge/license-Apache-blue',
+        GPL: 'https://img.shields.io/badge/license-GPL-lightgrey' 
+    }
+
+    return `# ${title}
+
+    ## Table of Contents
+    - [Description](#Description)
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Contribution](#Contribution)
+    - [Test](#Test)
+    - [Questions](#Questions)
+    
+
+    ![License](${choices[license]})
+
+    ## Description
+    
+    ${description}
+    
+    ## Installation
+
+    ${installation}
+
+    ## Usage
+
+    ${usage}
+
+    ## Contribution
+
+    ${contribution}
+
+    ## Test
+
+    ${test}
+
+    ## Questions
+
+    [Github](${github})
+
+    If you have any addtional questions, please contact me by email at: ${email}   
+    `;
 }
 
 inquirer
@@ -22,22 +68,23 @@ inquirer
             message: 'What are the steps to use this program?',
             name: 'usage'
         },
-        {   type: 'list',
+        {
+            type: 'list',
             message: 'Which license would you like to use?',
             name: 'license',
             choices: [
                 "MIT",
                 "Apache",
-                "MIT/Apache-2.0"
+                "GPL"
             ]
         },
         {
             message: 'What are the contribution guidelines?',
-            name: 'Constributing'
+            name: 'contribution'
         },
         {
             message: 'What are the test instructions?',
-            name: 'Tests'
+            name: 'test'
         },
         {
             message: 'What is your Github username?',
@@ -48,4 +95,7 @@ inquirer
             name: 'email'
         }
     ])
+    .then((answers) => {
+         fs.writeFile('ReadMePlease.md', readMe(answers) ,(err) => err ? console.error(err): console.log('Success!'));
+    });
 
