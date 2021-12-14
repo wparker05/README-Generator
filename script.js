fa8e4ec33171c937be3e8ee8e37f49bec72066c1
Wesley Rogers <wprogers86@gmail.com>
2021-12-13 21:20:23 -0500

    added prompt questions

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const readMe =({})=>{
+const readMe = ({ }) => {
 
-} 
+}
+
+inquirer
+    .prompt([
+        {
+            message: 'What is the name of your title?',
+            name: 'title'
+        },
+        {
+            message: 'Describe what your project is?',
+            name: 'description'
+        },
+        {
+            message: 'What are the steps required to install this project?',
+            name: 'installation'
+        },
+        {
+            message: 'What are the steps to use this program?',
+            name: 'usage'
+        },
+        {   type: 'list',
+            message: 'Which license would you like to use?',
+            name: 'license',
+            choices: [
+                "MIT",
+                "Apache",
+                "MIT/Apache-2.0"
+            ]
+        },
+        {
+            message: 'What are the contribution guidelines?',
+            name: 'Constributing'
+        },
+        {
+            message: 'What are the test instructions?',
+            name: 'Tests'
+        },
+        {
+            message: 'What is your Github username?',
+            name: 'github'
+        },
+        {
+            message: 'What is your email addres?',
+            name: 'email'
+        }
+    ])
 
